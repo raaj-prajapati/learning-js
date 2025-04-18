3f44551fdedbdba0070e9876ba439f9d22e721e8
raaj-prajapati <131648286+raaj-prajapati@users.noreply.github.com>
2025-04-18 08:13:00 +0000

    DataTypes

---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all Js Code as newer version
+
+//alert(3+3); //in node when you complie it, shows the error , and in brower console you do alert so you can get a output as 6
+
+// the javaScript engine is hiding in the brower;
+
+console.log(3 
+    +
+     3); //this is worng way to present you code, the code should be readability very high
+
+// Now Discuss about DataTypes, How many types of Data is Present 
+
+let Name = "Hitesh" // String DataTypes
+let age = 18 // Number datatype
+let isLoggedIn = false //boolean DataTypes
+
+// number => having there range like 2 to power 53; this much range is there, you got into a your memory address
+
+// bigint => mean if your number is much biger so we use the biginit, but gerenally we can't use it
+
+// String =>  "" in store in the double col 
+// boolean => true/false
+// null => standalone values null is specilly one type, null is reprentation of empty values, it is not undefined it is empty 
+// undefined => we can't aasing a values 
+// symbol => if we talk about uniques so we perfer the symbol
+
+//Object 
+
+
+
+
+
