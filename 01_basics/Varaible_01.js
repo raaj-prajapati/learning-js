3a05c879b241b3415bc4557d5e4bcc2abb1085a6
raaj-prajapati <131648286+raaj-prajapati@users.noreply.github.com>
2025-04-18 01:28:49 +0000

    contant

---
+++ 01_basics/Varaible_01.js
@@ -0,0 +1,11 @@
+const accountId = 1234; //memory name accountid = values we can asign is 1234;
+let accountEmail = "raj@google,com";
+var accountPassword = "1234";
+accountCity = "Jaipur";
+
+accountId = 2; //it will give an error because when we write a const keyword os we can't change it;  
+accountEmail = "harsh@gmail.com";
+accountPassword = "212121";
+accountCity = "Bangaluru";   
+
+console.log(accountId);  
