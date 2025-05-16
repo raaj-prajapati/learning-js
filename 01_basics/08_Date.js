709c6f09f9e5c2155686da58a9b5c4385b7320ce
raaj-prajapati <131648286+raaj-prajapati@users.noreply.github.com>
2025-05-16 20:49:39 +0000

    Date

---
+++ 01_basics/08_Date.js
@@ -0,0 +1,24 @@
+/*
+
+let myDate = new Date()
+console.log(myDate.toString()); //it will show the current time date and months 
+
+console.log(myDate.toDateString()); //output : Fri May 16 2025
+
+console.log(myDate.toLocaleString()); //5/16/2025, 8:30:42 PM
+//this all are having the specefic date;
+
+*/
+
+//CASE 1:
+//when i want to indentity a date so we used 
+console.log(typeof myDate); //the date is a object types
+
+let myCreateDate = new Date(2023, 0,23);
+console.log(myCreateDate.toDateString());
+
+
+
+
+
+
